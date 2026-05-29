4ea63602210455ec3f4fd10801cd455db789a809
Awesome-00 <sommoghe@gmail.com>
2026-05-29 17:55:08 +0530

    add basic shell

---
+++ shell.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT 1024
+#define MAX_ARGS 64
+
+void execute(char **args) {
+    pid_t pid = fork();
+    if (pid == 0) {
+        // child process
+        if (execvp(args[0], args) == -1) {
+            perror("error");
+        }
+        exit(1);
+    } else {
+        // parent waits
+        wait(NULL);
+    }
+}
+
+int main() {
+    char input[MAX_INPUT];
+    char *args[MAX_ARGS];
+
+    while (1) {
+        printf("myshell> ");
+        fflush(stdout);
+
+        if (!fgets(input, MAX_INPUT, stdin)) break;
+
+        input[strcspn(input, "\n")] = 0; // remove newline
+
+        if (strcmp(input, "exit") == 0) break;
+
+        // split input into args
+        int i = 0;
+        args[i] = strtok(input, " ");
+        while (args[i] != NULL) {
+            args[++i] = strtok(NULL, " ");
+        }
+
+        if (args[0] != NULL) execute(args);
+    }
+
+    return 0;
+}
